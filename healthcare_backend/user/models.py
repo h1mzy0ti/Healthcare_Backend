a12799535446ef61449aa1fc1aa04975daa7ebc8
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-07-24 15:18:17 +0530

    Add user app (includes jwt auth)

--- healthcare_backend/user/models.py
+++ healthcare_backend/user/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  
+    
+    def __str__(self):
+        return self.email
